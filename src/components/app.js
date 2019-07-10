7a5a965eb99f1fce535c1d6c17ecc17b7ca4b098
DallenBurwell <dallen.burwell@gmail.com>
2019-07-10 00:26:02 -0600

    Login Feature Implemented

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,9 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import axios from 'axios';
 
-import PortfolioContainer from "./portfolio/portfolio-container";
+
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home"
 import About from "./pages/about"
@@ -16,15 +17,81 @@ import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    })
+  }
+
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
+
+  checkLoginStatus() {
+    return axios.get("https://api.devcamp.space/logged_in", { 
+      withCredentials: true
+    }).then(response => {
+      const loggedIn = response.data.logged_in;
+      const loggedInStatus = this.state.loggedInStatus;
+
+      if (loggedIn && loggedInStatus === "LOGGED_IN") {
+        return loggedIn;
+      } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN"
+        });
+      } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN"
+        });
+      }
+    })
+    .catch(error => {
+      console.log("Error", error);
+    })
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
   render() {
     return (
       <div className='container'>
         <Router>
           <div>
             <NavigationContainer />
+            <h2>{this.state.loggedInStatus}</h2>
 
             <Switch>
               <Route exact path="/" component={Home} />
+
+              <Route 
+                path="/auth"
+                render={props => (
+                  <Auth 
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnSeccessfulLogin={this.handleUnSeccessfulLogin}
+                  />
+                )
+
+                }
+              />
               <Route path="/auth" component={Auth} />
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
