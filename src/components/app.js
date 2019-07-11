a1a2c1e62073720709ff162e48693b9c82ce9b03
DallenBurwell <dallen.burwell@gmail.com>
2019-07-10 22:05:39 -0600

    Added signout functionality and get call from api implemented

--- src/components/app.js
+++ src/components/app.js
@@ -12,6 +12,7 @@ import Home from "./pages/home"
 import About from "./pages/about"
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
@@ -26,7 +27,7 @@ export default class App extends Component {
 
     this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
     this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
-
+    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
   }
 
   handleSuccessfulLogin() {
@@ -36,6 +37,12 @@ export default class App extends Component {
   }
 
   handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
+
+  handleSuccessfulLogout() {
     this.setState({
       loggedInStatus: "NOT_LOGGED_IN"
     })
@@ -69,13 +76,20 @@ export default class App extends Component {
     this.checkLoginStatus();
   }
 
+  authorizedPages() {
+    return [
+      <Route path="/portfolio-manager" component={PortfolioManager} />
+    ]
+  }
   render() {
     return (
       <div className='container'>
         <Router>
           <div>
-            <NavigationContainer />
-            <h2>{this.state.loggedInStatus}</h2>
+            <NavigationContainer 
+            loggedInStatus={this.state.loggedInStatus}
+            handleSuccessfulLogout={this.handleSuccessfulLogout}
+            />
 
             <Switch>
               <Route exact path="/" component={Home} />
@@ -96,6 +110,7 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              {this.state.loggedInStatus === "LOGGED_IN" ?  this.authorizedPages() : null}
               <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
               <Route component={NoMatch} />
             </Switch>
---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+
+export default class PortfolioManager extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            portfolioItems: []
+        }
+    }
+
+getPortfolioItems() {
+    axios.get('https://dallenburwell.devcamp.space/portfolio/portfolio_items',
+    {withCredentials: true
+    })
+    .then( response => {
+        console.log("response from get portfolio items", response);
+        this.setState({
+            
+            portfolioItems: [...response.data.portfolio_items]
+        });
+    })
+    .catch(error => {
+        console.log("error in get portfolio items", error);
+    });
+}
+
+componentDidMount() {
+    this.getPortfolioItems();
+}
+
+    render() {
+        return (
+            <div className="portfolio-manager-wrapper">
+                <div className="left-column">
+                    <h1>Portfolio form...</h1>
+                </div>
+
+                <div className="right-column">
+                    <PortfolioSidebarList data={this.state.portfolioItems} />
+                </div>
+            </div>
+        )
+    }
+}
