91f8dca3f566d254fa240ff074fc2fb56704fe27
DallenBurwell <dallen.burwell@gmail.com>
2019-07-15 20:04:45 -0600

    blog data imported into project

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 class Blog extends Component {
     constructor() {
         super();
-    }
-    render() {
 
-    
+        this.state = {
+            blogItems: []
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this);
+    }
+
+getBlogItems() {
+    axios.get("https://dallenburwell.devcamp.space/portfolio/portfolio_blogs", { 
+    withCredentials: true }
+    ).then(response => {
+        this.setState({
+            blogItems: response.data.portfolio_blogs
+        })
+    }).catch(error => {
+        console.log("getBlogItems error", error);
+    })
+}
+
+componentWillMount() {
+    this.getBlogItems();
+}
+
+    render() {
+        const blogRecords = this.state.blogItems.map(blogItem => {
+            return <h1>{blogItem.title}</h1>;
+        });
         return (
             <div>
-                <h2>Blog</h2>
-
-                <div>
-                    <Link to="/about-me">Read more about me!</Link>
-                </div>
+               {blogRecords}
             </div>
         )
     }
