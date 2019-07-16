985a70e2f7da7b12f22b5cb792fa5a1075bef243
Dallen Burwell <40583585+DallenBurwell@users.noreply.github.com>
2019-07-16 12:02:19 -0600

    infinite scrolling

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -8,18 +8,34 @@ class Blog extends Component {
         super();
 
         this.state = {
-            blogItems: []
+            blogItems: [],
+            totalCount: 0,
+            currentPage: 0
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
+        this.activateInfiniteScroll();
+    }
+
+    activateInfiniteScroll() {
+        window.onscroll = () => {
+            if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+                console.log("get more posts");
+            }
+        }
     }
 
 getBlogItems() {
+    this.setState({
+        currentPage: this.state.currentPage + 1
+    });
+
     axios.get("https://dallenburwell.devcamp.space/portfolio/portfolio_blogs", { 
     withCredentials: true }
     ).then(response => {
         this.setState({
-            blogItems: response.data.portfolio_blogs
+            blogItems: response.data.portfolio_blogs,
+            totalCount: response.data.meta.total_records
         })
     }).catch(error => {
         console.log("getBlogItems error", error);
@@ -35,9 +51,13 @@ componentWillMount() {
             return <BlogItem key={blogItem.id} blogItem={blogItem} />
         });
         return (
-            <div>
-               {blogRecords}
+            <div className="blog-container">
+                <div className="content-container">
+                    {blogRecords}
+                </div>
             </div>
+               
+            
         )
     }
 }
