acaa0c1419ef73e657b5e5ed8652082c1652d35d
DallenBurwell <dallen.burwell@gmail.com>
2019-07-18 17:15:40 -0600

    modules installed

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ReactHtmlParser from "react-html-parser";
+import BlogFeaturedImage from '../blog/blog-featured-image';
 
 export default class BlogDetail extends Component {
     constructor(props) {
@@ -34,17 +36,14 @@ export default class BlogDetail extends Component {
             featured_image_url,
             blog_status
         } = this.state.blogItem;
-        console.log("currentID", this.state.currentId);
         return (
             <div className="blog-container">
                 <div className="content-container">
                     <h1>{title}</h1>
+                    
+                    <BlogFeaturedImage img={featured_image_url} />
 
-                    <div className="featured-image-wrapper">
-                        <img src={featured_image_url} />
-                    </div>
-
-                    <div className="content">{content}</div>
+                    <div className="content">{ReactHtmlParser(content)}</div>
                 </div>
             </div>
         )
